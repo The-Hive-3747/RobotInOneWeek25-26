7674069f548975af40420804e38e708f2e5d4da0
the hive 3747 <3747hive@gmail.com>
2025-09-30 16:27:49 -0600

    Started coding the Pedro TeleOp using the Example TeleOp -Tom

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/PedroTeleOp.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.pedro;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.HeadingInterpolator;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.util.function.Supplier;
+
+@Configurable
+@TeleOp
+public class PedroTeleOp extends OpMode {
+    private Follower follower;
+    public static Pose startingPose;
+    private boolean automatedDrive;
+    private Supplier<PathChain> pathChain;
+    private TelemetryManager telemetryM;
+    private boolean slowMode = false;
+    private double slowModeMultiplier = 0.5; //subject to change;
+
+    @Override
+    public void init(){
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startingPose == null ? new Pose() : startingPose);
+        follower.update();
+        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+
+        pathChain = () -> follower.pathBuilder()
+                .addPath(new Path(new BezierLine(follower::getPose, new Pose(45,98))))
+                //getPose, newPose subject to change
+                .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading,
+                        Math.toRadians(45),0.8)) //subject to change
+                .build();
+    }
+
+    @Override
+    public void start() {
+        
+    }
+}
