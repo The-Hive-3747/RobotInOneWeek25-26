2449336347d07431cc2aaa65fcf165e03d16b83c
the hive 3747 <3747hive@gmail.com>
2025-10-09 18:45:12 -0600

    uncommented some code for motors and added setup for the new motors and servo. Uses d-pad right and left for turret (not heavily tested). uncommented code for limelight to test position and a new variable for distance from center of apriltag, to be used for tracking the goal with turret.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/NextFTCTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/NextFTCTeleOp.java
@@ -2,14 +2,20 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import static dev.nextftc.bindings.Bindings.*;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import dev.nextftc.bindings.BindingManager;
 import dev.nextftc.bindings.Button;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.ftc.*;
 
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.teamcode.subsystems.FieldCentricDrive;
 import org.firstinspires.ftc.teamcode.helpers.GoBildaPinpointDriver;
 
@@ -28,6 +34,9 @@ public class NextFTCTeleOp extends NextFTCOpMode {
     double x;
     double y;
     double z;
+    double center;
+    double centerP;
+
     double horizDistance;
 
     @Override
@@ -37,25 +46,31 @@ public class NextFTCTeleOp extends NextFTCOpMode {
         Button g1Up = button(() -> gamepad1.dpad_up);
         Button g1Down = button(() -> gamepad1.dpad_down);
         Button g1LT = button(() -> gamepad1.left_trigger > 0.1);
+        Button g1Right = button(() -> gamepad1.dpad_right);
+        Button g1Left = button(() -> gamepad1.dpad_left);
 
-        /*Servo pivotServo = hardwareMap.get(Servo.class, "pivotServo");
+        /*Servo pivotServo = hardwareMap.get(Servo.class, "pivotServo");*/
         DcMotor shootMotor = hardwareMap.get(DcMotor.class, "shootMotor");
-        shootMotor.setDirection(DcMotorSimple.Direction.REVERSE);*/
+        DcMotor turretMotor = hardwareMap.get(DcMotor.class, "turretMotor");
+        Servo hoodServo = hardwareMap.get(Servo.class, "hoodServo");
+        shootMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
-        /*limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.pipelineSwitch(0);
         limelight.start();
+        telemetry.addData("Servo Position", hoodServo.getPosition());
 
         g1X.whenBecomesTrue(() -> shootMotor.setPower(SHOOT_POWER));
         g1Y.whenBecomesTrue(() -> shootMotor.setPower(0));
-        g1Up.whenBecomesTrue(() -> {
+
+        /*g1Up.whenBecomesTrue(() -> {
             SHOOT_POWER += 0.025;
             shootMotor.setPower(SHOOT_POWER);
         });
         g1Down.whenBecomesTrue(() -> {
             SHOOT_POWER -= 0.025;
             shootMotor.setPower(SHOOT_POWER);
-        });
+        });/*
         g1LT.toggleOnBecomesTrue()
                 .whenBecomesTrue(() -> {
                     pivotServo.setPosition(0.2);
@@ -63,25 +78,37 @@ public class NextFTCTeleOp extends NextFTCOpMode {
                 .whenBecomesFalse(() -> {
                     pivotServo.setPosition(0.6);
                 });*/
+        g1Left.whenTrue(() -> {
+            turretMotor.setPower(0.15);
+                });
 
+        g1Right.whenTrue(() -> {
+            turretMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            turretMotor.setPower(0.15);
+                });
     }
     @Override
     public void onUpdate() {
-        /*LLResult result = limelight.getLatestResult();
+        LLResult result = limelight.getLatestResult();
         if (result != null && result.isValid() && !result.getFiducialResults().isEmpty()) {
             LLResultTypes.FiducialResult fiducial = result.getFiducialResults().get(0);
             Pose3D botPose = fiducial.getRobotPoseTargetSpace();
+            centerP = fiducial.getTargetXPixels();
+            center = fiducial.getTargetXDegrees();
             y = botPose.getPosition().y;
             z = botPose.getPosition().z;
             x = botPose.getPosition().x;
+
             horizDistance = Math.sqrt(x*x + y*y);
-            //ActiveOpMode.telemetry().addData("horizontal distance", horizDistance);
+            ActiveOpMode.telemetry().addData("horizontal distance", horizDistance);
             ActiveOpMode.telemetry().addData("april tag", fiducial.getFiducialId());
             ActiveOpMode.telemetry().addData("z distance", z);
             ActiveOpMode.telemetry().addData("x distance", x);
             ActiveOpMode.telemetry().addData("y distance", y);
+            ActiveOpMode.telemetry().addData("center distance", center);
 
-        }*/
+
+        }
 
 
         BindingManager.update();
