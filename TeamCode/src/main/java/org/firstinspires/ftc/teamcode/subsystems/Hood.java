89cd9c7872d4f927a8f193cb782b76f70a2fc451
the hive 3747 <3747hive@gmail.com>
2025-11-13 19:41:16 -0700

    ts kinda goated (updating hood, ts raw code not in android studio i js text edited)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Hood.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Hood.java
@@ -10,21 +10,49 @@ import dev.nextftc.ftc.ActiveOpMode;
 public class Hood implements Component {
     CRServo hood;
     AnalogInput hoodEncoder;
+    static double hoodPos;
+    static double pastPos = 0;
+    static double currentPos;
+    static double loops;
+    static double targetPos;
+    ControlSystem hoodPID;
+    
     @Override
     public void postInit() {
         hood = ActiveOpMode.hardwareMap().get(CRServo.class, "hoodServo");
         hoodEncoder = ActiveOpMode.hardwareMap().get(AnalogInput.class, "hoodEncoder");
-
+        hoodPos = 0;
+        loops = 1;
+        ControlSystem hoodPID = ControlSystem.builder()
+            .posPID(0.01)
+            .build();
     }
 
+    public double getPos() {
+        return this.getRawPos()+(loops*360);
+    }
+
+    private double getRawPos() {
+        return hoodEncoder.getVoltage()/3.3)*360;
+    }
+
+    public void goToPos(double pos) {
+        targetPos = pos;
+        hoodPid.setGoal(
+            new KineticState(targetPos)
+            );
+    }
+
+
     public void update() {
-        if (ActiveOpMode.gamepad1().dpad_up) {
-            hood.setPower(1.0);
-        } else if (ActiveOpMode.gamepad1().dpad_down){
-            hood.setPower(-1);
-        } else {
-            hood.setPower(0.0);
-        }
+        hood.setPower(hoodPid.calculate(
+            new KineticState(getPos())
+        ));
+        
+        currentPos = getRawPos();
+        if (pastPos == 0) { pastPos = currentPos; }
+        if (pastPos - currentPos > 100) { loops++; }
+        
         ActiveOpMode.telemetry().addData("encoder current pos", (hoodEncoder.getVoltage()/3.3)*360);
         ActiveOpMode.telemetry().addData("encoder max pos", hoodEncoder.getMaxVoltage());
     }
