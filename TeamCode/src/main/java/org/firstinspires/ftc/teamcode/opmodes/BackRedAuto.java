06b4f743854c5e241b4a04d2454fcc4bb593f4ec
nesetkab <foulofdoom@gmail.com>
2026-01-13 13:12:42 -0700

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BackRedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BackRedAuto.java
@@ -1,54 +1,21 @@
-
 package org.firstinspires.ftc.teamcode.opmodes;
 
-import static org.firstinspires.ftc.teamcode.opmodes.BackAutoPaths.intake1;
-import static org.firstinspires.ftc.teamcode.opmodes.BackAutoPaths.intake2;
-import static org.firstinspires.ftc.teamcode.opmodes.BackAutoPaths.intake3;
-import static org.firstinspires.ftc.teamcode.opmodes.BackAutoPaths.lineUpForIntake1;
-import static org.firstinspires.ftc.teamcode.opmodes.BackAutoPaths.lineUpForIntake2;
-import static org.firstinspires.ftc.teamcode.opmodes.BackAutoPaths.lineUpForIntake3;
-import static org.firstinspires.ftc.teamcode.opmodes.BackAutoPaths.park;
-import static org.firstinspires.ftc.teamcode.opmodes.BackAutoPaths.startAngle;
-import static org.firstinspires.ftc.teamcode.opmodes.BackAutoPaths.startingPose;
-import static org.firstinspires.ftc.teamcode.opmodes.BackAutoPaths.toShootFromIntake1;
-import static org.firstinspires.ftc.teamcode.opmodes.BackAutoPaths.toShootFromIntake2;
-import static org.firstinspires.ftc.teamcode.opmodes.BackAutoPaths.toShootFromIntake3;
-import static org.firstinspires.ftc.teamcode.opmodes.BackAutoPaths.toShootFromStart;
-
-import com.bylazar.telemetry.PanelsTelemetry;
-import com.bylazar.telemetry.TelemetryManager;
-import com.pedropathing.geometry.Pose;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-
-import org.firstinspires.ftc.teamcode.subsystems.Aimbot;
-import org.firstinspires.ftc.teamcode.subsystems.Hood;
-import org.firstinspires.ftc.teamcode.subsystems.TurretLights;
-import org.firstinspires.ftc.teamcode.utilities.Alliance;
-import org.firstinspires.ftc.teamcode.utilities.Light;
-import org.firstinspires.ftc.teamcode.utilities.OpModeTransfer;
-import org.firstinspires.ftc.teamcode.pathing.Constants;
-import org.firstinspires.ftc.teamcode.subsystems.Flywheel;
-import org.firstinspires.ftc.teamcode.subsystems.Intake;
-import org.firstinspires.ftc.teamcode.subsystems.Turret;
-
-import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.commands.delays.Delay;
-import dev.nextftc.core.commands.groups.CommandGroup;
-import dev.nextftc.core.commands.groups.ParallelGroup;
-import dev.nextftc.core.commands.groups.SequentialGroup;
-import dev.nextftc.core.commands.utility.InstantCommand;
-import dev.nextftc.extensions.pedro.FollowPath;
-import dev.nextftc.extensions.pedro.PedroComponent;
-import com.pedropathing.follower.Follower;
-import dev.nextftc.ftc.NextFTCOpMode;
 
 @Autonomous(name = "back red auto")
 public class BackRedAuto extends AutoTemplate {
   @Override
   public void preInit() {
     super.startingPose = new Pose(86.5,8.62,90);
-  super.alliance = Alliance.RED;
-  // super.autonomousCommands ;
-
+    super.alliance = Alliance.RED;
+    super.autonomousCommands = new SequentialGroup(
+                startIntakeFlywheelAndTurret, 
+                new Delay(5),
+                followPathAndShoot(toShootFromStart),
+                followPathAndIntake(lineUpForIntake1, intake1),
+                followPathAndShoot(toShootFromIntake1),
+                followPathAndIntake(lineUpForIntake2, intake2),
+                followPathAndShoot(toShootFromIntake2),
+                followPathAndPark(park) 
+        );
   }
-  }
+}
