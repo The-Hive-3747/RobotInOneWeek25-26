a25dce9b6226ac2eca0aa82af6e7ea49ebb80c9b
the hive 3747 <3747hive@gmail.com>
2026-01-20 18:52:39 -0700

    Converted to Pedro's coordinate system for the limelight so we can start coding the relocalization function.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Relocalization.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Relocalization.java
@@ -1,12 +1,19 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import dev.nextftc.core.components.Component;
+import dev.nextftc.core.units.Distance;
 import dev.nextftc.ftc.ActiveOpMode;
 
+import com.pedropathing.ftc.InvertedFTCCoordinates;
+import com.pedropathing.ftc.PoseConverter;
+import com.pedropathing.geometry.PedroCoordinates;
+import com.pedropathing.geometry.Pose;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.limelightvision.LLResult;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 
 public class Relocalization implements Component{
@@ -24,11 +31,19 @@ Limelight3A limelight;
     if (result != null && result.isValid()) {
         Pose3D botPose = result.getBotpose();
         if (botPose != null) {
-            double x = botPose.getPosition().x;
-            double y = botPose.getPosition().y;
+            //Convert the Pose3D to a Pose2D in order to convert to Pedro
+            Pose2D botPose2D = new Pose2D(DistanceUnit.INCH ,botPose.getPosition().x, botPose.getPosition().y, AngleUnit.RADIANS ,botPose.getOrientation().getYaw());
+            //Convert the Pose2D into Pedro Pose
+            Pose botPosePedro = PoseConverter.pose2DToPose(botPose2D, InvertedFTCCoordinates.INSTANCE);
+            //Make the coordinate system to Pedro's coordinate system.
+            botPosePedro.getAsCoordinateSystem(PedroCoordinates.INSTANCE);
+            double x = botPosePedro.getX();
+            double y = botPosePedro.getY();
+            double heading = botPosePedro.getHeading();
 
             ActiveOpMode.telemetry().addData("Bot X", x);
             ActiveOpMode.telemetry().addData("Bot Y", y);
+            ActiveOpMode.telemetry().addData("Bot Heading", Math.toDegrees(heading));
             ActiveOpMode.telemetry().addData("Bot pos", botPose.getPosition().toUnit(DistanceUnit.INCH));
 
         }
